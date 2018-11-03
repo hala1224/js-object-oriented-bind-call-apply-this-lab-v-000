240bc9690c55422ebf13856add3d104c7d5480eb
Natalie Halawani <hala1224@gmail.com>
2018-11-03 23:27:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 //Your code here
+function justInvoke(func) {
+  return func();
+}
+
+function setThisWithCall(fn, thisValue, arg) {
+  
+  return fn.call(thisValue, arg);
+}
+
+function setThisWithApply(fn, thisValue, args) {
+  
+  return fn.apply(thisValue, args);
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  
+  function copy(arg) {
+     this.value=functionToBeCopied(arg);
+
+  }
+  return copy;
+
+
+  
+}
